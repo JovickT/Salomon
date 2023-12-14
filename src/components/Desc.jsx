8f916e5b3591356081845ad29336b40bafe48fc5
Mehdiii91 <mehdi.triaa2901@gmail.com>
2023-12-14 15:32:41 +0100

    desc

--- src/components/Desc.jsx
+++ src/components/Desc.jsx
@@ -2,14 +2,21 @@
 const Desc = () => {
     return (
         <>
-        <div class="card" style={{width: "18rem"}}>
-            <img  src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fstore.epicgames.com%2Ffr%2Fp%2Fea-sports-fc-24--ultimate-edition&psig=AOvVaw2hb1kDKmrhMsnl3KhLxOlz&ust=1702649874073000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCNj3t5qPj4MDFQAAAAAdAAAAABAD" alt=""  class="card-img-top" />
+        <div>
+            <br />
+            <br />
+            <h1 class="d-flex justify-content-center">Description des jeux vidéos.</h1>
+            <br /><br />
+            <div class="card" style={{width: "20rem"}}>
+            <img  src="https://cdn1.epicgames.com/offer/4750c68b2bfa4f43933b81cfd5cc510c/EGS_EASPORTSFC24UltimateEdition_EACanada_Editions_S2_1200x1600-dc25e6d71666959d43074b7ed2868ad7" alt="fc 24"  class="card-img-top" style={{}} />
         <div class="card-body">
             <h5 class="card-title">FC 24</h5>
             <p class="card-text">EA Sports FC 24 est un jeu vidéo de football développé par Electronic Arts et édité par EA Sports, sorti le 29 septembre 2023 sur Microsoft Windows, PlayStation 4, PlayStation 5, Xbox One, Xbox Series et Nintendo Switch.</p>
             <a href="#" class="btn btn-primary">S'inscire au tournoi</a>
         </div>
         </div>
+        </div>
+        
         </>
     );
 };

c25c8b3dce91eee0ad42588625378a93d510af0f
JovickT <jovicktchakala@yahoo.com>
2023-12-16 15:12:59 +0100

    -mise à jour du main

--- src/components/Desc.jsx
+++ src/components/Desc.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useRef, useState } from "react";
 import requestHttp from "../services/api_service.js"
+import '../assets/css/Desc.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 import Nav from "./Nav.jsx";
